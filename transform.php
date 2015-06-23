7697928474797139192d26e0f0697a4ab3131138
Mike <michaeljones401@gmail.com>
2015-06-23 17:16:35 +0100

    started work on php script to process the data

--- transform.php
+++ transform.php
@@ -3,12 +3,11 @@
 class CommanetTransform {
 
   private $csv_path;
-  private $commaet_tables = [
-    'score','rem','LPlan','comms','album','albums','audio','passwords',
-    'harbour','photo','donor','1','2','3','4', '5', '6', '7', '8', '9', 
-    '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 
-    '21', '22', '23'
-  ];
+  private $commaet_tables = ['photo','13','15','17','rem'];
+
+  private $items = [];
+
+  private $locations = [];
 
   public function __construct()
   {
@@ -16,17 +15,156 @@ class CommanetTransform {
     
     $this->csv_path = $argv[1];
 
-    $this->getCsvFiles();
+    // Create an array from all the photos
+    $this->createPhotosArray();
+
+    // Add dates to the photos
+    $this->addDates();
+
+    // Add a title to the photos
+    $this->addTitle();
+
+    // Add author and description to the photos
+    $this->addAuthorDescription();
+ 
+    // Add a location to the photos
+    $this->addLocation();
+  }
+
+
+  /**
+   *
+   */
+  private function createPhotosArray()
+  {
+    $data = $this->csv_to_array($this->csv_path . '/photo.csv');
+
+    foreach($data as $d) {
+      $this->items[$d['recid']]['file'] = $d['photo_file'];
+    }
+  }
+
+
+  /**
+   *
+   */
+  private function addDates()
+  {
+    $data = $this->csv_to_array($this->csv_path . '/13.csv');
+    foreach($data as $d) {
+      $this->items[$d['recid']]['date'] = $d['item'];
+    }
+  }
+
+
+  /**
+   *
+   */
+  private function addLocation()
+  {
+    $data = $this->csv_to_array($this->csv_path . '/15.csv');
+
+    foreach($data as $d) {
+      $location = $d['item'];
+      if(in_array($location, $this->locations)) {
+        echo 'noooo';
+        $this->items[$d['recid']]['location'] = ['location' => $location , 'lat_lng' => $this->locations[$location]];
+      }else{
+        $geo_data = $this->getLatLng($location);
+        $this->locations[$location] = $geo_data;
+        $this->items[$d['recid']]['location'] = ['location' => $location , 'lat_lng' => $geo_data];
+      }
+    }
   }
 
   /**
-   * Loop through all the csv files
+   *
    */
-  public function getCsvFiles()
+  private function addTitle()
   {
-    foreach($this->commaet_tables as $tables) {
-      echo file_get_contents($this->csv_path . '/' . $tables . '.csv');
+    $data = $this->csv_to_array($this->csv_path . '/17.csv');
+    foreach($data as $d) {
+      $this->items[$d['recid']]['title'] = $d['item'];
     }
+  }
+
+
+  /**
+   *
+   */
+  private function addAuthorDescription()
+  {
+    $data = $this->csv_to_array($this->csv_path . '/rem.csv');
+    foreach($data as $d) {
+      $this->items[$d['recid']]['author'] = $d['author'];
+      $this->items[$d['recid']]['description'] = $d['RemText'];
+    }
+  }
+
+
+  /**
+   * Convert a comma separated file into an associated array.
+   * The first row should contain the array keys.
+   * 
+   * Example:
+   * 
+   * @param string $filename Path to the CSV file
+   * @param string $delimiter The separator used in the file
+   * @return array
+   * @link http://gist.github.com/385876
+   * @author Jay Williams <http://myd3.com/>
+   * @copyright Copyright (c) 2010, Jay Williams
+   * @license http://www.opensource.org/licenses/mit-license.php MIT License
+   */
+  private function csv_to_array($filename='', $delimiter=',')
+  {
+    if(!file_exists($filename) || !is_readable($filename))
+      return FALSE;
+    
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+      while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+      {
+        if(!$header)
+          $header = $row;
+        else
+          $data[] = array_combine($header, $row);
+      }
+      fclose($handle);
+    }
+    return $data;
+  }
+
+
+
+  /**
+   *
+   * @param $place string - place name
+   * @param return string - lat lng of the placename
+   */
+  private function getLatLng($place)
+  {
+    sleep(2);
+
+    $url = 'https://maps.googleapis.com/maps/api/geocode/json?address='.$place.'%20wales&sensor=false';
+
+    $ch = curl_init();
+    $timeout = 0;
+    curl_setopt ($ch, CURLOPT_URL, $url);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $file_contents = curl_exec($ch);
+    curl_close($ch);
+
+    $geoArr = json_decode($file_contents,true);
+    // echo '<pre>' , print_r($geoArr) , '</pre>';
+    $geoLocation = $geoArr['results'][0]['geometry']['location'];
+    $lat = $geoLocation['lat'];
+    $lng = $geoLocation['lng'];
+
+    return $lat.','.$lng;
   }
 
 }
